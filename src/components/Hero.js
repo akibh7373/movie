ce359ca8266141b5f0ed73c1195a437656927b83
Akib Hossain <akib.hossain737@gmail.com>
2023-09-12 13:29:13 -0700

    ADDED: Hero section modified

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,8 @@ export default function Hero() {
     return (
         <>
             <div className="hero flex flex-col justify-center items-center px-5 md:pb-[50px] pb-5 pt-[80px] text-center bg-slate-700 text-white">
-                <h1>Downlad movies: HD smallest size</h1>
-                <p className="lg:mt-2 mt-3">Welcome to official YTS website. Here you can browse and download YTFI movies in excellent. <span className="md:block sm:inline-block lg:pt-2">720p, 1080p, 2160p 4K and 3D quality, all at the smallest file size. YTS Movies Torrents.</span></p>
+                <h1 className="capitalize">All Movies One Platform</h1>
+                <p className="lg:mt-2 mt-3">Best movies of all time. Here you can browse and download movies in excellent.</p>
             </div>
         </>
     )
