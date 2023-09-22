3ede617cf122854952a5a56dc06d65208039300e
Akib Hossain <akib.hossain737@gmail.com>
2023-09-21 22:51:22 -0700

    ADDED: Home

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
-
+import Btn from "@/utlis/Buttons";
 export default function Hero() {
     return (
         <>
-            <div className="hero flex flex-col justify-center items-center px-5 md:pb-[50px] pb-5 pt-[80px] radial-bg text-center text-white">
-                <h1 className="capitalize">All Movies One Platform</h1>
-                <p className="lg:mt-2 mt-3">Best movies of all time. Here you can browse and download movies in excellent.</p>
-                <Link href='' className="lg:my-[50px] px-4 py-2 uppercase font-bold rounded-3xl bg-yellow text-zinc-900">Streaming</Link>
+            <div className="hero flex flex-col justify-center items-center px-5 md:h-[70vh] h-[50vh] radial-bg text-center text-white">
+                <h1 className="uppercase font-bold">All Movies One Platform</h1>
+                <p className="my-4">Best movies of all time. Here you can browse and download movies in excellent.</p>
+                <Link href='#' className="mt-4 font-bold text-black"><Btn title="streaming" /></Link>
             </div>
         </>
     )
