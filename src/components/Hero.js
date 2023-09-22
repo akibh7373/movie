d229f0d10d891b20aa4ed27b8cc0530bdcf9b3e3
Akib Hossain <akib.hossain737@gmail.com>
2023-09-21 23:07:46 -0700

    ADDED: Home

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import Btn from "@/utlis/Buttons";
 export default function Hero() {
     return (
         <>
-            <div className="hero flex flex-col justify-end items-center px-5 pb-[30px] md:h-[70vh] h-[50vh] radial-bg text-center text-white">
+            <div className="hero flex flex-col justify-end items-center px-5 pb-[60px] md:h-[70vh] h-[70vh] radial-bg text-center text-white">
                 <h1 className="uppercase font-bold">All Movies One Platform</h1>
                 <p className="my-4">Best movies of all time. Here you can browse and download movies in excellent.</p>
                 <Link href='#' className="mt-4 font-bold text-black"><Btn title="streaming" /></Link>
